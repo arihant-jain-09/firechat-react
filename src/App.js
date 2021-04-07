86494e34df3284485b2086d0a3d63fbc8ae5da52
arihant jain <jain.ari2000@gmail.com>
2021-04-07 12:25:47 +0530

    fetch and display documents having 2 fields createdAt and text

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 firebase.initializeApp({
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+  apiKey: "AIzaSyBWZC9xWMKEQMuGPyncAFlaVGnHlNwnEDY",
+    authDomain: "superchat-33ed4.firebaseapp.com",
+    projectId: "superchat-33ed4",
+    storageBucket: "superchat-33ed4.appspot.com",
+    messagingSenderId: "305060583764",
+    appId: "1:305060583764:web:f11e117b65ec65e718d0c1",
+    measurementId: "G-45GES8K7BV"
 })
 const firestore=firebase.firestore();
 const auth=firebase.auth();
@@ -19,10 +19,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <img src='Added_a_document.png' width="500" height="500"/>
       </header>
       <section>
-      {user?<FetchFirestoreDB/>:<SignIn/>}
+      {user?<ChatRoom/>:<SignIn/>}
       {console.log(user)}
       {/* {when the user is logged in we can capture image of his gmail photo using user object which has photoURL} */}
       {/* {auth which comes from firebase/auth has access to currentUser so we can check if user is logged in or not} */}
@@ -47,9 +47,9 @@ function SignOut(){
     <button onClick={()=>auth.signOut()}>SignOut</button>
   )
 }
-function FetchFirestoreDB(){
+function ChatRoom(){
 const messageRef=firestore.collection('messages');
-const query=messageRef.limit(25);
+const query=messageRef.orderBy('createdAt').limit(25);
 //here we will use useCollectionData hook to look for changes in real time
 //it returns an object where each object is the chat message in database
 //Synatx useCollectionData(query,options)
@@ -60,13 +60,17 @@ return(
   <>
     <div>
       {/* if messages then only map */}
-      {messages && messages.map((msg)=><DisplayMesage key={msg.id} id={msg.id} message={msg}/>)}
+      {messages && messages.map((msg)=><ChatMessage key={msg.id} id={msg.id} message={msg}/>)}
     </div>
   </>
 )
 }
-function DisplayMesage(props){
+function  ChatMessage  (props){
+  const {text}=props.message;
 console.log(props.message);
-return(<div></div>)
+return(
+  <div><p>{text}</p>
+</div>
+)
 }
 export default App;
